b333262a75175e79f6f88f62acae4975e40b97cc
Isaev.N <nikitavegas95@gmail.com>
2023-09-01 06:51:16 +0300

    API Firebase Registration Login user

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import {Route, Routes} from "react-router-dom";
 import {FC} from "react";
 import HomePage from "./pages/Home-page/Home-page.tsx";
 import RegistrationPage from "./pages/Registration-page/Registration-page.tsx";
+import MainPage from "./pages/Main-page/MainPage.tsx";
 
 const App:FC = () => (
  <Routes>
      <Route path='/' element={<HomePage/>}/>
      <Route path='/registration' element={<RegistrationPage />}/>
+     <Route path='/main' element={<MainPage />}/>
  </Routes>
 );
 
--- src/moduls/form/components/Registration-form/Registration-form.tsx
+++ src/moduls/form/components/Registration-form/Registration-form.tsx
@@ -1,19 +1,26 @@
 import {FC} from 'react';
 import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 import FormReg from "../../../../components/Form/Form.tsx";
-import {Simulate} from "react-dom/test-utils";
-import error = Simulate.error;
+import {useNavigate} from "react-router-dom";
+// import {useDispatch} from "react-redux";
 
 const RegistrationForm:FC = () => {
+    // const dispatch = useDispatch();
+    const navigation = useNavigate()
+
     const handleRegister = (email:string, password:string) => {
         const auth = getAuth();
         createUserWithEmailAndPassword(auth, email, password)
-            .then()
-            .catch(error)
+            .then(() => {
+                alert('Вы зарегистрировались')
+                navigation('/')
+            })
+            .catch(() => {
+                alert('Что-то делаете не так')
+            })
     }
     return (
         <FormReg title='Регистрация' handleClick={handleRegister} />
-
     );
 };
 

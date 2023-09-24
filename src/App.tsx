eebbc4e151075465a104c057c483e8faa5304845
Isaev.N <nikitavegas95@gmail.com>
2023-09-24 15:40:42 +0300

    login / registration email toLowerCase

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import {FC} from "react";
 import HomePage from "./pages/Home-page/Home-page.tsx";
 import RegistrationPage from "./pages/Registration-page/Registration-page.tsx";
 import MainPage from "./pages/Main-page/MainPage.tsx";
-// import {useDispatch} from "react-redux";
-// import {AppDispatch} from "./store";
-// import fetchAuthMe from "./store/thunk/fetchAuthMe.ts";
 
 const App:FC = () => {
 

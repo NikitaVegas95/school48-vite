7cecff471926be012e4cfdf894275a88dece6416
Isaev.N <nikitavegas95@gmail.com>
2023-11-29 00:08:48 +0300

    build server deploy

--- src/axios.ts
+++ src/axios.ts
@@ -2,15 +2,15 @@ import axios from 'axios'
 
 // для деплоя
 
-// const instance:any = axios.create({
-//     baseURL: 'https://server-tan-seven.vercel.app'
-// })
+const instance:any = axios.create({
+    baseURL: 'https://server-tan-seven.vercel.app/'
+})
 
 // local
-
-const instance:any = axios.create({
-    baseURL: 'http://localhost:7412/'
-})
+//
+// const instance:any = axios.create({
+//     baseURL: 'http://localhost:7412/'
+// })
 
 instance.interceptors.request.use((config: any) => {
     config.headers.Authorization = window.localStorage.getItem('token')
